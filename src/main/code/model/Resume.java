f7ff74bab900a3bbb4e2f4acb8b046d6c5fbc808
Bohdan Stepanov <bohdan.math.stepanov@gmail.com>
2019-01-27 14:53:45 +0200

    add equals and hashCode methods

--- src/main/code/model/Resume.java
+++ src/main/code/model/Resume.java
@@ -16,6 +16,18 @@ public class Resume {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return uuid.equals(((Resume) o).uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
     public String toString() {
         return uuid;
     }
