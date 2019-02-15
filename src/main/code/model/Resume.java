d94b75b46a0354963ae3425961ba524856f179d6
Bohdan Stepanov <bohdan.math.stepanov@gmail.com>
2019-02-15 22:33:26 +0200

    remove model basic unused methods

--- src/main/code/model/Resume.java
+++ src/main/code/model/Resume.java
@@ -31,24 +31,6 @@ public class Resume {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Resume resume = (Resume) o;
-
-        if (!uuid.equals(resume.uuid)) return false;
-        return fullName.equals(resume.fullName);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = uuid.hashCode();
-        result = 31 * result + fullName.hashCode();
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "Resume{" +
                 "uuid='" + uuid + '\'' +
