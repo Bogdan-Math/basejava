26273d211c1d78766fb99d395fc2293460fbe988
Bohdan Stepanov <bohdan.math.stepanov@gmail.com>
2019-02-20 18:58:05 +0200

    add section type

---
+++ src/main/code/model/SectionType.java
@@ -0,0 +1,21 @@
+package main.code.model;
+
+public enum SectionType {
+
+    PERSONAL("Personal"),
+    OBJECTIVE("Objective"),
+    ACHIEVEMENT("Achievement"),
+    QUALIFICATION("Qualification"),
+    EXPERIENCE("Experience"),
+    EDUCATION("Education");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
