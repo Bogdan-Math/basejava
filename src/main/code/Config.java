48c8682556fdf457c3c98ce45e34959165c4c02a
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2019-04-26 16:12:40 +0300

    add properties config

---
+++ src/main/code/Config.java
@@ -0,0 +1,49 @@
+package main.code;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+
+    private static final Config INSTANCE = new Config();
+
+    private String pathName;
+    private String dbUrl;
+    private String dbUser;
+    private String dbPassword;
+
+    public static Config getInstance() {
+        return INSTANCE;
+    }
+
+    private Config() {
+        try (InputStream is = new FileInputStream("./config/resume.properties")) {
+            Properties properties = new Properties();
+            properties.load(is);
+            pathName = properties.getProperty("path.name");
+            dbUrl = properties.getProperty("db.url");
+            dbUser = properties.getProperty("db.user");
+            dbPassword = properties.getProperty("db.password");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getPathName() {
+        return pathName;
+    }
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+}
--- test/main/code/storage/disk/AbstractDiskStorageTest.java
+++ test/main/code/storage/disk/AbstractDiskStorageTest.java
@@ -1,11 +1,12 @@
 package main.code.storage.disk;
 
+import main.code.Config;
 import main.code.storage.AbstractStorageTest;
 import main.code.storage.Storage;
 
 public abstract class AbstractDiskStorageTest extends AbstractStorageTest {
 
-    protected static final String PATH_NAME = "storage";
+    protected static final String PATH_NAME = Config.getInstance().getPathName();
 
     protected AbstractDiskStorageTest(Storage storage) {
         super(storage);
